29824504a3b19226bf5995983b67d756a880bac4
KW <kenneth_sl_wong@msn.com>
2022-02-17 13:38:08 -0500

    added structure of promises to init function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// TODO: Include packages needed for this application
 const fs = require('fs');
+const inquirer = require('inquirer');
+const generateLicense = require('./utils/generateLicense');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
@@ -8,8 +9,25 @@ const questions = [];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// App init function
+function init() {
+    questions()
+    .then(answers => {
+        return generateLicense(answers);
+    })
+    .then(answers => {
+        return generateMarkdown(answers);
+    })
+    .then(pageMarkdown => {
+        return writeToFile('./dist/README.md',pageMarkdown);
+    })
+    .then(writeResponse => {
+        console.log(writeResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
 
 // Function call to initialize app
 init();
